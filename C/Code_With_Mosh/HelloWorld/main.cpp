454275f3c9d872df418dab550d03e5f2ad6c9466
khanhNVM <minhbi245@gmail.com>
2024-04-08 14:52:53 +0700

    Created folder learn C with Mosh

---
+++ C/Code_With_Mosh/HelloWorld/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // import library file using function print out the screen
+
+int main() {
+    std::cout << "Helloooo world!!!!"; // using std:cout the function will print the value to the console
+    return 0;   // this will inform the compiler that program run exactly and don't have any errors
+}
