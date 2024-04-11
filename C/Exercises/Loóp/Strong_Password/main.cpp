5a300e8d824b529b2977262e4f07592d6bed4f82
khanhNVM <minhbi245@gmail.com>
2024-04-11 17:26:53 +0700

    Analyze the IPO to handle the input strong Password

--- C/Exercises/Loóp/Strong_Password/main.cpp
+++ C/Exercises/Loóp/Strong_Password/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+/*
+    input: enter the password is a string
+    process:
+        - check the password have lenght is at least 6
+        - check the password contain at least 1 digits
+        - check the password contains at least one lowercase English character
+        - check the password contains at least one uppercase
+        - check the password contains at least one specical character
+
+    output 
+*/
+
 int main() {
 
     return 0;
